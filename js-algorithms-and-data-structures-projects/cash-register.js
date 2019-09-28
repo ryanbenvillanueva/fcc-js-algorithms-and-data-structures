6db9ace3bcd7a5a1de10cd9718c7d66dec6fb616
Ryan Ben S. Villanueva <ryanben.villanueva@gmail.com>
2019-09-28 21:52:03 +0800

    Solve the cash-register-challenge

--- js-algorithms-and-data-structures-projects/cash-register.js
+++ js-algorithms-and-data-structures-projects/cash-register.js
@@ -31,48 +31,52 @@
 // checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])should return {status: "INSUFFICIENT_FUNDS", change: []}.
 // checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])should return {status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]}.
 
-const currencyUnits = [{
-        name: 'ONE HUNDRED',
-        amount: 100.00
+let currencyUnits = [{
+        name: "ONE HUNDRED",
+        value: 100.00
     },
     {
-        name: 'TWENTY',
-        amount: 20.00
+        name: "TWENTY",
+        value: 20.00
     },
     {
-        name: 'TEN',
-        amount: 10.00
+        name: "TEN",
+        value: 10.00
     },
     {
-        name: 'FIVE',
-        amount: 5.00
+        name: "FIVE",
+        value: 5.00
     },
     {
-        name: 'ONE',
-        amount: 1.00
+        name: "ONE",
+        value: 1.00
     },
     {
-        name: 'QUARTER',
-        amount: 0.25
+        name: "QUARTER",
+        value: 0.25
     },
     {
-        name: 'DIME',
-        amount: 0.10
+        name: "DIME",
+        value: 0.10
     },
     {
-        name: 'NICKEL',
-        amount: 0.05
+        name: "NICKEL",
+        value: 0.05
     },
     {
-        name: 'PENNY',
-        amount: 0.01
+        name: "PENNY",
+        value: 0.01
     }
 ];
 
 function checkCashRegister(price, cash, cid) {
-    const change = cash - price;
-    const totalCashInDrawer = cid.reduce((accumulator, currentValue) => accumulator + currentValue[i][1], 0.0);
-    
+    let change = cash - price;
+    let totalCashInDrawer = 0;
+
+    for (let i = 0; i < cid.length; i++) {
+        totalCashInDrawer += cid[i][1];
+    }
+
     if (totalCashInDrawer < change) {
         return {
             status: "INSUFFICIENT_FUNDS",
@@ -86,36 +90,37 @@ function checkCashRegister(price, cash, cid) {
     }
 
     cid = cid.reverse();
-    let  changeResult = currencyUnits.reduce(function(accumulator, currentValue, index) {
-        if (change >= currentValue.amount) {
-            let totalAmount = 0.0;
-            while (change > currentValue.amount && cid[index][i][1] >= currentValue.amount) {
-                totalAmount += currentValue.amount;
-                change -= currentValue.amount;
-                change = Math.round(change * 100) / 100;
-                cid[index[i]] -= currentValue.amount;
-            } 
-            accumulator.push([currentValue.name, totalAmount]);
-            return accumulator;
-        } else {
-            return accumulator;
+    let output = currencyUnits.reduce(function (accumulator, next, index) {
+        let currentValue = 0.0;
+        while (change >= next.value && cid[index][1] > 0) {
+            change -= next.value;
+            cid[index][1] -= next.value;
+            currentValue += next.value;
+
+            change = Math.round(change * 100) / 100;
+
         }
+        if (currentValue > 0) {
+            accumulator.push([next.name, currentValue]);
+        }
+        return accumulator;
     }, []);
 
-    // Here is your change, ma'am.
-    return change;
-}
+    console.log("output: " + output);
 
-// Example cash-in-drawer array:
-// [["PENNY", 1.01],
-// ["NICKEL", 2.05],
-// ["DIME", 3.1],
-// ["QUARTER", 4.25],
-// ["ONE", 90],
-// ["FIVE", 55],
-// ["TEN", 20],
-// ["TWENTY", 60],
-// ["ONE HUNDRED", 100]]
+    if (output.length < 1 || change > 0) {
+        return {
+            status: "INSUFFICIENT_FUNDS",
+            change: []
+        };
+    }
+
+    // Here is your change, ma'am.
+    return {
+        status: "OPEN",
+        change: output
+    };
+}
 
 checkCashRegister(19.5, 20, [
     ["PENNY", 1.01],
